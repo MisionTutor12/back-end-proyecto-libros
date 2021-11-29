813a75a00141cd037299f46089c0126b17e6e109
oneCiser <41345659+oneCiser@users.noreply.github.com>
2021-11-29 16:01:20 -0500

    add env variable

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ dbConnect();
 app.use(Express.json());
 app.use(cors());
 app.use('/app', router);
-const puerto = 5000;
+const puerto = process.env.PORT || 3000;
 app.listen(puerto,()=>{
     console.log(`Servidor corriendo en el puerto ${puerto}`);
 });
