6d896bde7b5fdc51146705ebf4bc9b23e3d5e9a4
oneCiser <41345659+oneCiser@users.noreply.github.com>
2021-11-29 16:38:53 -0500

    fix bugs

--- src/routes/libro.js
+++ src/routes/libro.js
@@ -4,9 +4,10 @@ const router = Router();
 
 router.get('/libros', (req, res) => Libro.getAll(req, res));
 router.get('/libro/:serial', (req, res) => Libro.getOne(req,res));
+
+router.put('/libro/:serial', (req, res) => Libro.update(req,res));
+router.delete('/libro/:serial', (req, res) => Libro.remove(req, res));
 router.post('/libro', (req, res) => Libro.create(req,res));
-router.put('/libro/:serial', (req, res) => update(req,res));
-router.delete('libro/:serial', (req, res) => Libro.remove(req, res));
 
 
 module.exports = router;
